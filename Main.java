a0268882eef3a6eb60e0496e47549329d43fb551
Burhanudin Hawari <87931411+hawari123@users.noreply.github.com>
2022-10-24 13:56:42 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main { 
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            if(x % 2 == 0){
+                System.out.println("angka "+ x +" adalah genap");
+            }else{
+                System.out.println("angka "+ x +" adalah ganjil");
+            }
+        }
+    }
+}
